86e110a41d12c1277bb6179618eb48ce95f0d8e7
Anber <ambersattarmh@gmail.com>
2024-02-13 18:14:03 +0500

    first commit

--- Tailwind CSS -13/tailwind.config.js
+++ Tailwind CSS -13/tailwind.config.js
@@ -1,17 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*"],
   theme: {
+    screens: {
+      xsm: '500px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
-      spacing:{
+      spacing: {
         13: '3.25rem'
       },
-      fontSize:{
+      fontSize: {
         '10xl': ['9rem', { lineHeight: '1.2' }],
       }
     },
   },
   plugins: [],
 }
-
-
